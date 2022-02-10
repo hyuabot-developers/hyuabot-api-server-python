6703a1639f609ded6802b8201bc2f1ea2509cefb
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-02-10 16:42:02 +0900

    feat: load subway timetable into before run server

--- tests/test_initialize_data.py
+++ tests/test_initialize_data.py
@@ -4,7 +4,7 @@ import aioredis
 import pytest
 
 from app.hyuabot.api.core.config import settings
-from app.hyuabot.api.initialize_data import load_shuttle_timetable, load_bus_timetable
+from app.hyuabot.api.initialize_data import load_shuttle_timetable, load_bus_timetable, load_subway_timetable
 
 
 @pytest.mark.asyncio
@@ -57,3 +57,32 @@ async def test_store_bus_timetable():
                     hour, minute = bus_time.split(":")
                     assert 0 <= int(hour) < 24
                     assert 0 <= int(minute) < 60
+
+
+@pytest.mark.asyncio
+async def test_store_subway_timetable():
+    await load_subway_timetable()
+
+    line_keys = ["skyblue", "yellow"]
+    day_keys = ["weekdays", "weekends"]
+    heading_keys = ["up", "down"]
+    redis_client = aioredis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}")
+
+    async with redis_client.client() as connection:
+        for line in line_keys:
+            for day in day_keys:
+                for heading in heading_keys:
+                    key = f"subway_{line}_{day}_{heading}"
+                    json_string: bytes = await connection.get(key)
+                    timetable: list[dict] = json.loads(json_string.decode("utf-8"))
+
+                    assert len(timetable) > 0
+                    for subway_item in timetable:
+                        assert "terminalStation" in subway_item.keys()
+                        assert "departureTime" in subway_item.keys()
+
+                        assert ":" in subway_item["departureTime"]
+                        hour, minute, seconds = subway_item["departureTime"].split(":")
+                        assert 0 <= int(hour) < 24
+                        assert 0 <= int(minute) < 60
+                        assert 0 <= int(seconds) < 60
