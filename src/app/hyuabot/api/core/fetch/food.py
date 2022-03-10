4e8b22c05058903b89ce496d0679f6d1d3bcce45
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-03-10 15:57:55 +0900

    fix: SSL requests error when fetch restaurant

--- src/app/hyuabot/api/core/fetch/food.py
+++ src/app/hyuabot/api/core/fetch/food.py
@@ -1,16 +1,12 @@
-import ssl
-
-import aiohttp
-import certifi
-from fastapi import APIRouter
 import asyncio
 import json
 
+import grequests as grequests
 from bs4 import BeautifulSoup
+from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
 from app.hyuabot.api.core.database import get_redis_connection, set_redis_value
-
 
 fetch_restaurant_menu_router = APIRouter(prefix="/food")
 restaurant_id_dict = {
@@ -30,42 +26,41 @@ restaurant_id_dict = {
 
 @fetch_restaurant_menu_router.get("", status_code=200)
 async def fetch_reading_room() -> JSONResponse:
+    urls = []
+    for restaurant_url_key in restaurant_id_dict.values():
+        urls.append(f"https://www.hanyang.ac.kr/web/www/re{restaurant_url_key}")
+    responses = [grequests.get(u) for u in urls]
+
     tasks = []
-    for restaurant_key, restaurant_url_key in restaurant_id_dict.items():
-        tasks.append(fetch_restaurant_menu_by_id(restaurant_key, restaurant_url_key))
+    for restaurant_key, response in zip(restaurant_id_dict.keys(), grequests.map(responses)):
+        tasks.append(fetch_restaurant_menu_by_id(restaurant_key, response))
     await asyncio.gather(*tasks)
     return JSONResponse({"message": "Fetch restaurant menu data success"}, status_code=200)
 
 
-async def fetch_restaurant_menu_by_id(restaurant_key: str, restaurant_url_key: str) -> None:
-    url = f"https://www.hanyang.ac.kr/web/www/re{restaurant_url_key}"
-    conn = aiohttp.TCPConnector(verify_ssl=False)
+async def fetch_restaurant_menu_by_id(restaurant_key: str, response) -> None:
+    cafeteria_info = {}
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    async with aiohttp.ClientSession(connector=conn) as session:
-        async with session.get(url) as response:
-            cafeteria_info = {}
-            soup = BeautifulSoup(await response.text(), "html.parser")
-
-            for inbox in soup.find_all("div", {"class": "tab-pane"}):
-                for content in inbox.find_all("td"):
-                    txt = content.text.strip()
-                    cafeteria_info['time'] = ''
-                    if '조식' in txt:
-                        cafeteria_info['time'] += f'{txt}\n'
-                    elif '중식' in txt:
-                        cafeteria_info['time'] += f'{txt}\n'
-                    elif '석식' in txt:
-                        cafeteria_info['time'] += f'{txt}\n'
-            for inbox in soup.find_all("div", {"class": "in-box"}):
-                title = inbox.find("h4").text.strip()
-                cafeteria_info[title] = []
-                for list_item in inbox.find_all("li"):
-                    if list_item.find("p", {"class": "price"}):
-                        menu = list_item.find("h3").text.replace("\t", "").replace("\r\n", "")
-                        p = list_item.find("h3").text
-                        cafeteria_info[title].append({"menu": menu, "price": p})
-            print(cafeteria_info)
-            redis_connection = await get_redis_connection("restaurant")
-            await set_redis_value(redis_connection, restaurant_key,
-                                  json.dumps(cafeteria_info, ensure_ascii=False).encode("utf-8"))
-            await redis_connection.close()
+    for inbox in soup.find_all("div", {"class": "tab-pane"}):
+        for content in inbox.find_all("td"):
+            txt = content.text.strip()
+            cafeteria_info['time'] = ''
+            if '조식' in txt:
+                cafeteria_info['time'] += f'{txt}\n'
+            elif '중식' in txt:
+                cafeteria_info['time'] += f'{txt}\n'
+            elif '석식' in txt:
+                cafeteria_info['time'] += f'{txt}\n'
+    for inbox in soup.find_all("div", {"class": "in-box"}):
+        title = inbox.find("h4").text.strip()
+        cafeteria_info[title] = []
+        for list_item in inbox.find_all("li"):
+            if list_item.find("h3"):
+                menu = list_item.find("h3").text.replace("\t", "").replace("\r\n", "")
+                p = list_item.find("p", {"class": "price"}).text
+                cafeteria_info[title].append({"menu": menu, "price": p})
+    redis_connection = await get_redis_connection("restaurant")
+    await set_redis_value(redis_connection, restaurant_key,
+                          json.dumps(cafeteria_info, ensure_ascii=False).encode("utf-8"))
+    await redis_connection.close()
--- src/app/hyuabot/api/main.py
+++ src/app/hyuabot/api/main.py
@@ -1,7 +1,13 @@
+import asyncio
+import sys
+
 from hypercorn.config import Config
 
 from app.hyuabot.api import create_app, AppSettings
 
+py_ver = int(f"{sys.version_info.major}{sys.version_info.minor}")
+if py_ver > 37 and sys.platform.startswith('win'):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 hypercorn_config = Config()
 app_settings = AppSettings()
 app = create_app(app_settings)
