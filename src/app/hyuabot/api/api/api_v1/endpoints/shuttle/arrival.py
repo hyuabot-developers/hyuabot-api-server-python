b4e24836b71ea23350d72512ecd61cef63d52631
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-06-22 18:45:50 +0900

    refactor: 셔틀버스 도착 정보 조회를 postgresql에 맞게 수정

--- src/app/hyuabot/api/api/api_v1/endpoints/shuttle/arrival.py
+++ src/app/hyuabot/api/api/api_v1/endpoints/shuttle/arrival.py
@@ -1,15 +1,18 @@
 import asyncio
-import json
 from datetime import datetime, timedelta
 from typing import Tuple
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy import and_
+from sqlalchemy.orm import Session
 
 from app.hyuabot.api.api.api_v1.endpoints.shuttle import shuttle_stop_type
 from app.hyuabot.api.core.date import get_shuttle_term, korea_standard_time
+from app.hyuabot.api.models.postgresql import shuttle as db
 from app.hyuabot.api.schemas.shuttle import ShuttleDepartureByStop, ShuttleDepartureItem, \
     ShuttleDeparture, ShuttleDepartureTimetable, ShuttleTimetableOnly
+from app.hyuabot.api.utlis.fastapi import get_db_session
 
 arrival_router = APIRouter(prefix="/arrival")
 shuttle_stop_dict = {"Dormitory": "기숙사", "Shuttlecock_O": "셔틀콕", "Station": "한대앞",
@@ -21,38 +24,38 @@ def compare_timetable(shuttle_time: datetime, now: datetime) -> bool:
     return shuttle_time > now
 
 
-async def fetch_timetable_by_stop(shuttle_stop: str, current_term: str,
+async def fetch_timetable_by_stop(db_session: Session, shuttle_stop: str, period: str,
                                   weekdays_keys: str, get_all: bool = False) -> \
         Tuple[list[ShuttleDepartureItem], list[ShuttleDepartureItem]]:
     now = datetime.now(tz=korea_standard_time)
-    key = f"shuttle_{current_term}_{weekdays_keys}"
-    # timetable: list[dict] = json.loads(json_string.decode("utf-8"))
-    timetable = []
+    timetable = db_session.query(db.ShuttleTimetable).filter(and_(
+        db.ShuttleTimetable.weekday == weekdays_keys,
+        db.ShuttleTimetable.period == period,
+    )).all()
     shuttle_for_station: list[ShuttleDepartureItem] = []
     shuttle_for_terminal: list[ShuttleDepartureItem] = []
-
-    for shuttle_index, shuttle_time in enumerate(timetable):
-        shuttle_departure_time = datetime.strptime(shuttle_time["time"], "%H:%M") \
-            .replace(tzinfo=korea_standard_time)
-        shuttle_heading = shuttle_time["type"]
-        if shuttle_stop == "Dormitory" and shuttle_time["startStop"] == "Dormitory":
+    for shuttle_item in timetable:  # type: db.ShuttleTimetable
+        shuttle_departure_time = now.replace(
+            hour=shuttle_item.shuttle_time.hour, minute=shuttle_item.shuttle_time.minute)
+        shuttle_heading = shuttle_item.shuttle_type
+        if shuttle_stop == "Dormitory" and shuttle_item.start_stop == "Dormitory":
             timedelta_minute = -5
             shuttle_departure_time += timedelta(minutes=timedelta_minute)
             if get_all or compare_timetable(shuttle_departure_time, now):
                 if shuttle_heading == "DH":
                     shuttle_for_station.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
                 elif shuttle_heading == "DY":
                     shuttle_for_terminal.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
                 elif shuttle_heading == "C":
                     shuttle_for_station.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
                     shuttle_for_terminal.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
         elif shuttle_stop == "Shuttlecock_O":
             timedelta_minute = 0
@@ -60,18 +63,18 @@ async def fetch_timetable_by_stop(shuttle_stop: str, current_term: str,
             if get_all or compare_timetable(shuttle_departure_time, now):
                 if shuttle_heading == "DH":
                     shuttle_for_station.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
                 elif shuttle_heading == "DY":
                     shuttle_for_terminal.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
                 elif shuttle_heading == "C":
                     shuttle_for_station.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
                     shuttle_for_terminal.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
         elif shuttle_stop == "Station":
             timedelta_minute = 10
@@ -79,14 +82,14 @@ async def fetch_timetable_by_stop(shuttle_stop: str, current_term: str,
             if get_all or compare_timetable(shuttle_departure_time, now):
                 if shuttle_heading == "DH":
                     shuttle_for_station.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
                 elif shuttle_heading == "C":
                     shuttle_for_station.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
                     shuttle_for_terminal.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
         elif shuttle_stop == "Terminal":
             if shuttle_heading == "DY":
@@ -94,16 +97,16 @@ async def fetch_timetable_by_stop(shuttle_stop: str, current_term: str,
                 shuttle_departure_time += timedelta(minutes=timedelta_minute)
                 if get_all or compare_timetable(shuttle_departure_time, now):
                     shuttle_for_terminal.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
             elif shuttle_heading == "C":
                 timedelta_minute = 15
                 shuttle_departure_time += timedelta(minutes=timedelta_minute)
                 if get_all or compare_timetable(shuttle_departure_time, now):
                     shuttle_for_terminal.append(ShuttleDepartureItem(
-                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                        time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                     ))
-        elif shuttle_stop == "Shuttlecock_I" or shuttle_time["startStop"] == "Dormitory":
+        elif shuttle_stop == "Shuttlecock_I" or shuttle_item.start_stop == "Dormitory":
             if shuttle_heading == "DH" or shuttle_heading == "DY":
                 timedelta_minute = 20
             else:
@@ -111,14 +114,14 @@ async def fetch_timetable_by_stop(shuttle_stop: str, current_term: str,
             shuttle_departure_time += timedelta(minutes=timedelta_minute)
             if get_all or compare_timetable(shuttle_departure_time, now):
                 shuttle_for_terminal.append(ShuttleDepartureItem(
-                    time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_time["type"],
+                    time=shuttle_departure_time.strftime("%H:%M"), type=shuttle_item.shuttle_type,
                 ))
     return shuttle_for_station, shuttle_for_terminal
 
 
 @arrival_router.get("", status_code=200, response_model=ShuttleDeparture)
-async def fetch_arrival_list():
-    is_working, current_term, weekdays_keys, query_time = await get_shuttle_term()
+async def fetch_arrival_list(db_session: Session = Depends(get_db_session)):
+    is_working, current_term, weekdays_keys, query_time = await get_shuttle_term(db_session)
     if not is_working:
         return JSONResponse(content={
             "message": "당일은 셔틀을 운행하지 않습니다.",
@@ -127,7 +130,7 @@ async def fetch_arrival_list():
 
     tasks = []
     for shuttle_stop in shuttle_stop_dict.keys():
-        tasks.append(fetch_timetable_by_stop(shuttle_stop, current_term, weekdays_keys))
+        tasks.append(fetch_timetable_by_stop(db_session, shuttle_stop, current_term, weekdays_keys))
 
     results = []
     for (shuttle_stop_id, shuttle_stop_name), (shuttle_for_station, shuttle_for_terminal) \
@@ -145,18 +148,18 @@ async def fetch_arrival_list():
 
 
 @arrival_router.get("/{shuttle_stop}", status_code=200, response_model=ShuttleDepartureByStop)
-async def fetch_arrival_list_by_stop(shuttle_stop: str):
+async def fetch_arrival_list_by_stop(shuttle_stop: str, db_session: Session = Depends(get_db_session)):
     if shuttle_stop not in shuttle_stop_type:
         return JSONResponse(status_code=404, content={"message": "존재하지 않는 셔틀버스 정류장입니다."})
 
-    is_working, current_term, weekdays_keys, query_time = await get_shuttle_term()
+    is_working, current_term, weekdays_keys, query_time = await get_shuttle_term(db_session)
     if not is_working:
         return JSONResponse(content={
             "message": "당일은 셔틀을 운행하지 않습니다.",
             "busForStation": [], "busForTerminal": [],
         })
     shuttle_for_station, shuttle_for_terminal = \
-        await fetch_timetable_by_stop(shuttle_stop, current_term, weekdays_keys)
+        await fetch_timetable_by_stop(db_session, shuttle_stop, current_term, weekdays_keys)
     return ShuttleDepartureByStop(
         stopName=shuttle_stop_dict[shuttle_stop],
         stopCode=shuttle_stop,
@@ -166,18 +169,19 @@ async def fetch_arrival_list_by_stop(shuttle_stop: str):
 
 
 @arrival_router.get("/{shuttle_stop}/timetable", status_code=200, response_model=ShuttleDepartureByStop)
-async def fetch_timetable_list_by_stop(shuttle_stop: str):
+async def fetch_timetable_list_by_stop(shuttle_stop: str, db_session: Session = Depends(get_db_session)):
     if shuttle_stop not in shuttle_stop_type:
         return JSONResponse(status_code=404, content={"message": "존재하지 않는 셔틀버스 정류장입니다."})
 
-    is_working, current_term, weekdays_keys, query_time = await get_shuttle_term()
+    is_working, current_term, weekdays_keys, query_time = await get_shuttle_term(db_session)
     if not is_working:
         return JSONResponse(content={
             "message": "당일은 셔틀을 운행하지 않습니다.",
             "busForStation": [], "busForTerminal": [],
         })
     shuttle_for_station, shuttle_for_terminal = \
-        await fetch_timetable_by_stop(shuttle_stop, current_term, weekdays_keys, get_all=True)
+        await fetch_timetable_by_stop(
+            db_session, shuttle_stop, current_term, weekdays_keys, get_all=True)
     return ShuttleDepartureByStop(
         stopName=shuttle_stop_dict[shuttle_stop],
         stopCode=shuttle_stop,
@@ -188,15 +192,16 @@ async def fetch_timetable_list_by_stop(shuttle_stop: str):
 
 @arrival_router.get("/{shuttle_stop}/timetable/all", status_code=200,
                     response_model=ShuttleDepartureTimetable)
-async def fetch_timetable_list_by_stop_all(shuttle_stop: str):
+async def fetch_timetable_list_by_stop_all(
+        shuttle_stop: str, db_session: Session = Depends(get_db_session)):
     if shuttle_stop not in shuttle_stop_type:
         return JSONResponse(status_code=404, content={"message": "존재하지 않는 셔틀버스 정류장입니다."})
 
-    is_working, current_term, _, query_time = await get_shuttle_term()
+    is_working, current_term, _, query_time = await get_shuttle_term(db_session)
     shuttle_for_station_weekdays, shuttle_for_terminal_weekdays = \
-        await fetch_timetable_by_stop(shuttle_stop, current_term, "weekdays", get_all=True)
+        await fetch_timetable_by_stop(db_session, shuttle_stop, current_term, "weekdays", get_all=True)
     shuttle_for_station_weekends, shuttle_for_terminal_weekends = \
-        await fetch_timetable_by_stop(shuttle_stop, current_term, "weekends", get_all=True)
+        await fetch_timetable_by_stop(db_session, shuttle_stop, current_term, "weekends", get_all=True)
     return ShuttleDepartureTimetable(
         stopName=shuttle_stop_dict[shuttle_stop],
         stopCode=shuttle_stop,
--- src/app/hyuabot/api/core/date.py
+++ src/app/hyuabot/api/core/date.py
@@ -4,55 +4,55 @@ from typing import Tuple
 
 import holidays
 from korean_lunar_calendar import KoreanLunarCalendar
+from sqlalchemy import and_
+from sqlalchemy.orm import Session
+
+from app.hyuabot.api.models.postgresql.shuttle import ShuttlePeriod
 
 korea_standard_time = timezone(timedelta(hours=9))
 
 
-async def get_shuttle_term(date: datetime = None) -> Tuple[bool, str, str, str]:
+async def get_shuttle_term(db_session: Session, date_item: datetime = None) \
+        -> Tuple[bool, str, str, str]:
     """
     오늘 날짜에 대한 셔틀 운행 타입을 반환합니다.
-    :param date: 날짜
+    :param db_session:
+    :param date_item:
     :return:
     셔틀 운행 여부(True/False),
     셔틀 운행 타입(semester, vacation, vacation_session)
     """
-    if date is None:
-        date = datetime.now(tz=korea_standard_time)
+    if date_item is None:
+        date_item = datetime.now(tz=korea_standard_time)
 
     calendar = KoreanLunarCalendar()
-    calendar.setSolarDate(date.year, date.month, date.day)
+    calendar.setSolarDate(date_item.year, date_item.month, date_item.day)
+    period_item = db_session.query(ShuttlePeriod)\
+        .filter(and_(
+            date_item >= ShuttlePeriod.start_date,
+            date_item <= ShuttlePeriod.end_date,
+            ShuttlePeriod.calendar_type == "solar"))\
+        .order_by(ShuttlePeriod.end_date - ShuttlePeriod.start_date).first()
+    if period_item is None:
+        return True, "semester", "weekays", date_item.strftime("%Y-%m-%d %H:%M:%S")
 
     # 운행 여부 확인
     is_working = True
-    # if f"{date.month}/{date.day}" in date_json["halt"]["solar"]:
-    #     is_working = False
-    # elif f"{calendar.lunarMonth}/{calendar.lunarDay}" in date_json["halt"]["lunar"]:
-    #     is_working = False
+    if period_item.period == "halt":
+        is_working = False
 
-    # 운행 타입 확인
-    term_keys = ["semester", "vacation", "vacation_session"]
-    # current_term = ""
-    # for term_key in term_keys:
-    #     for term in date_json[term_key]:
-    #         start_date = datetime.strptime(term["start"], "%m/%d")\
-    #             .replace(year=date.year, tzinfo=korea_standard_time)
-    #         end_date = datetime.strptime(term["end"], "%m/%d")\
-    #             .replace(year=date.year, tzinfo=korea_standard_time)
-    #         if start_date >= end_date:
-    #             if date >= start_date:
-    #                 start_date = start_date.replace(year=date.year)
-    #                 end_date = end_date.replace(year=date.year + 1)
-    #             elif date <= end_date:
-    #                 start_date = start_date.replace(year=date.year - 1)
-    #                 end_date = end_date.replace(year=date.year)
-    #         if start_date <= date <= end_date:
-    #             current_term = term_key
-    #             break
-    #     if current_term:
-    #         break
+    lunar_period_item = db_session.query(ShuttlePeriod)\
+        .filter(and_(
+            date_item >= ShuttlePeriod.start_date,
+            date_item <= ShuttlePeriod.end_date,
+            ShuttlePeriod.calendar_type == "lunar",
+            ShuttlePeriod.period == "halt",))\
+        .order_by(ShuttlePeriod.end_date - ShuttlePeriod.start_date).first()
+    if lunar_period_item is not None:
+        is_working = False
 
-    if date.weekday() >= 5 or date.strftime("%Y-%m-%d") in holidays.KR():
+    if date_item.weekday() >= 5 or date_item.strftime("%Y-%m-%d") in holidays.KR():
         weekday_key = "weekends"
     else:
         weekday_key = "weekdays"
-    return is_working, "current_term", weekday_key, date.strftime("%Y-%m-%d %H:%M:%S")
+    return is_working, period_item.period, weekday_key, date_item.strftime("%Y-%m-%d %H:%M:%S")
