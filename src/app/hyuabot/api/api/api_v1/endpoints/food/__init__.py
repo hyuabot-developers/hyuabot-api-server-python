c36f6842b4d88c8d13f65f64da8b7addac503559
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-06-22 16:28:18 +0900

    refactor: 캠퍼스 별 학식 조회를 sqlalchemy에 맞게 수정

--- src/app/hyuabot/api/api/api_v1/endpoints/food/__init__.py
+++ src/app/hyuabot/api/api/api_v1/endpoints/food/__init__.py
@@ -1,8 +1,8 @@
 # Query params
 from fastapi import APIRouter
 
-from app.hyuabot.api.api.api_v1.endpoints.food.restaurant import restaurant_router
 from app.hyuabot.api.api.api_v1.endpoints.food.campus import restaurant_menu_campus_router
+from app.hyuabot.api.api.api_v1.endpoints.food.restaurant import restaurant_router
 
 restaurant_menu_router = APIRouter(prefix="/food")
 restaurant_menu_router.include_router(restaurant_menu_campus_router)
---
+++ src/app/hyuabot/api/api/api_v1/endpoints/food/convert.py
@@ -0,0 +1,11 @@
+from app.hyuabot.api.models.postgresql import cafeteria as db
+from app.hyuabot.api.schemas.cafeteria import MenuItem
+
+
+def convert_menu_item(menu_items: list[db.Menu]) -> dict[str, list[MenuItem]]:
+    menu_dict = {}
+    for menu_item in menu_items:
+        if menu_item.time_type not in menu_dict:
+            menu_dict[menu_item.time_type] = []
+        menu_dict[menu_item.time_type].append(MenuItem(menu_item.menu_description, menu_item.price))
+    return menu_dict
